d97407c2be1298889dc48383cf1141f34307633f
Ayush Tiwari <01tiwari.ayush@gmail.com>
2023-09-07 21:01:31 +0530

    update with error detection

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const searchBtn = document.querySelector(".search-btn");
 const searchText = document.querySelector(".search-box");
 const apikey = "4938edcfef648165a938c68faa59af33";
-let cityName = "pilkhuwa";
-checkWeather();
 async function checkWeather() {
-  if (searchText.value != null) cityName = searchText.value;
-  else cityName = "pilkhuwa";
+  const cityName = searchText.value;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=metric`;
+  console.log(url);
   const response = await fetch(url);
-  searchText.value = "";
   let data = await response.json();
   console.log(data);
-  const temp = data.main.temp;
-  const humidity = data.main.humidity;
-  const windSpeed = data.wind.speed;
-  document.querySelector(".city").innerText = cityName;
-  document.querySelector(".temp").innerText = temp + "°c";
-  document.querySelector(".humidity").innerText = humidity + "%";
-  document.querySelector(".wind").innerText = windSpeed + "km/hr";
+  if (response.status === 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    document.querySelector(".error").style.display = "none";
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+    const weatherIcon = data.weather[0].icon;
+    const imgSrc = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+    document.querySelector(".weather-icon").setAttribute("src", imgSrc);
+    document.querySelector(".city").innerText = cityName;
+    document.querySelector(".temp").innerText = temp + "°c";
+    document.querySelector(".humidity").innerText = humidity + "%";
+    document.querySelector(".wind").innerText = windSpeed + " km/hr";
+
+    document.querySelector(".weather").style.display = "block";
+  }
+  searchText.value = "";
 }
 
 // checkWeather();
